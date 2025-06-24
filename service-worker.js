f978e86a0fdd4936b8bdc914fe7af6cef90b589a
jafar733612 <jafarsalh773240@gmail.com>
2025-06-24 16:00:26 +0300

    Update service-worker.js
    تفعيل الكاش للعمل بدون إنترنت

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,42 @@
-
-const CACHE_NAME = 'xljoys-cache-v1';
+const CACHE_NAME = "xljoys-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/script.js',
-  '/images/logo-xljoys.png',
-  '/assets/logo.png',
-  '/admin_panel.html',
-  '/owners/login.html',
-  '/owner/manage.html'
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/logo.png",
+  "/logo-xljoys.png"
 ];
 
-self.addEventListener('install', function(event) {
+// تثبيت الكاش
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+// تفعيل الخدمة
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// جلب الملفات
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
